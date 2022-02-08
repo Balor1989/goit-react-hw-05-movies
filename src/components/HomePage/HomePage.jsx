986a62950cb2f4cd9df846ca2302344ba678a594
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-02-08 13:46:37 +0200

    adds styles to HomePage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-// import s from './HomePage.module.css';
+import s from './HomePage.module.css';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { fetchPopularMovies } from '../../services/fetchMovies';
@@ -11,23 +11,24 @@ const HomePage = () => {
     fetchPopularMovies().then(setPopularMovies);
   }, []);
   return (
-    <>
+    <section className={s.homePage}>
       <HTag title="Trending Today" />
-      <ul>
+      <ul className={s.movieList}>
         {popularMovies &&
           popularMovies.results.map(movie => (
-            <li key={movie.id}>
+            <li key={movie.id} className={s.movieCard}>
               <Link to={`/movies/${movie.id}`}>
                 <img
                   src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
                   alt="{movie.title}"
+                  width="100px"
                 />
-                {movie.title}
+                <h3 className={s.movieTitle}>{movie.title}</h3>
               </Link>
             </li>
           ))}
       </ul>
-    </>
+    </section>
   );
 };
 
