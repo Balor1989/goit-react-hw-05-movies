06558077e20802a6c378123efa62551683ed6134
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-02-10 15:33:29 +0200

    fixs movies page

--- src/views/MoviesPage/MoviesPage.jsx
+++ src/views/MoviesPage/MoviesPage.jsx
@@ -1,26 +1,46 @@
 import MoviesList from '../../components/MoviesList';
 import s from './MoviesPage.module.css';
 import { GoSearch } from 'react-icons/go';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchSearchMovies } from '../../services/fetchMovies';
 import { toast } from 'react-toastify';
 import { IconContext } from 'react-icons';
+import { useSearchParams } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 
 const MoviesPage = () => {
   const [searchMovies, setSearchMovies] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams({});
   const [query, setQuery] = useState('');
+
+  const queryValue = searchParams.get('query');
+
+  useEffect(() => {
+    if (!queryValue) {
+      return;
+    }
+    fetchSearchMovies(queryValue).then().then(setSearchMovies);
+  }, [queryValue]);
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    fetchSearchMovies(query).then().then(setSearchMovies);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchParams]);
 
   const onHandleChange = e => {
     setQuery(e.currentTarget.value);
   };
+
   const onQuerySubmit = e => {
     e.preventDefault();
     if (!query) {
       toast.warn('Please, enter movie name');
       return;
     }
-    fetchSearchMovies(query).then().then(setSearchMovies);
+    setSearchParams({ query });
   };
 
   return (
